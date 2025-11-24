ef8eea694d11b12b4061af8396a9a18f30fe7e82
Keshav Bhardwaj <keshav_bhardwaj@hcltech.com>
2025-11-24 15:42:39 +0530

    added files to the repo

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+
 # Read existing corporate RG; do NOT create a new one
 data "azurerm_resource_group" "rg" {
   name = var.rg_name
@@ -14,20 +15,34 @@ resource "azurerm_storage_account" "this" {
   location                 = data.azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-  min_tls_version          = "TLS1_2"
-  tags                     = var.tags
+
+  # ---- Policy compliance settings ----
+  public_network_access_enabled = false
+  min_tls_version               = "TLS1_2"
+
+  network_rules {
+    default_action = "Deny"
+    # Keep these empty unless your policy explicitly allows them
+    # ip_rules                   = []
+    # virtual_network_subnet_ids = []
+    # If policy forbids bypass, do not add:
+    # bypass = ["AzureServices"]
+  }
+
+  # Uncomment if policy enforces this in your org
+  # allow_shared_key_access = false
+
+  tags = var.tags
 }
 
-
 module "resource1" {
-  source = "./modules/resource1"
+  source  = "./modules/resource1"
   rg_name = "Keshav_Bhardwaj_RG"
 }
 
 module "resource2" {
-  source = "./modules/resource2"
-  rg_name = "Keshav_Bhardwaj_RG"
+  source   = "./modules/resource2"
+  rg_name  = "Keshav_Bhardwaj_RG"
   location = "eastus"
-  sa_name = "keshav24novsa"
+  sa_name  = "keshav24novsa"
 }
-
