3ac08d73c010980a7875e0fca4e3e49e0f095065
Keshav Bhardwaj <keshav_bhardwaj@hcltech.com>
2025-11-24 15:25:26 +0530

    added files to the repo

--- provider.tf
+++ provider.tf
@@ -9,15 +9,6 @@ terraform {
 
 // provider.tf (root)
 provider "azurerm" {
+  subscription_id = "679f3d56-bed2-429f-9e31-4d7bf67e14c7"
   features {}
-
-  // Optional: if you want to hardcode (better to use env/vars)
-  // subscription_id = var.subscription_id
-}
-
-// If you choose to pass it via variable:
-// variables.tf (root)
-variable "subscription_id" {
-  type        = string
-  description = "679f3d56-bed2-429f-9e31-4d7bf67e14c7"
 }
